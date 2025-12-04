1f8591e1747ad1ae8ebaa658c64646dab3a0f721
PREETCHAUHAN2005 <preet.24b0101197@abes.ac.in>
2025-12-04 21:11:06 +0530

    1st message

---
+++ notes.md
@@ -0,0 +1,7 @@
+dependencies - anuy package on which project is dependent  
+  
+  Homework-[alt text](image.png)
+
+  difference between caret and tilda (^ vs ~)
+  what are dependencies 
+  what is the use of -g while npm install
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use("/hello",(req,res) => {
+    res.send("Hello Preet!");
+});
+
+
+app.use("/test",(req,res) => {
+    res.send("Hello from  the server!");
+});
+
+app.listen(7777,()=> {
+    console.log("Server is running on  port 7777")
+});
