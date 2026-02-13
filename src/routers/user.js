d534ae3acf6b9c761dcedc701d6ea79957c178c2
PREETCHAUHAN2005 <preet.24b0101197@abes.ac.in>
2026-02-13 11:25:49 +0530

    feat: initialize devtinder-frontend with Vite and React setup
    - Added package.json with scripts and dependencies for React and Vite
    - Included Vite logo SVG in the public directory
    - Created main App component with a simple header
    - Added global styles in index.css for consistent theming
    - Set up main entry point in main.jsx to render the App component
    - Configured Vite with a basic setup in vite.config.js
    - Enhanced user feed endpoint to paginate results and filter hidden users

--- src/routers/user.js
+++ src/routers/user.js
@@ -54,6 +54,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
     // A person shoud not see in his feed include
     // 1.Card of his own
     // card of people whom he sent request already
@@ -61,8 +62,32 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     // 4.card of people who sent request to him but he rejected or acceted
     // 5.his connections
 
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
+    const connectionRequest = await ConnectionRequests.fnd({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
 
+    const hidenUsersFromFeed = new set();
+    connectionRequest.find((req) => {
+      hidenUsersFromFeed.add(req.fromUserId.toString());
+      hidenUsersFromFeed.add(req.toUserId.toString());
+    });
+    console.log("hidenUsersFromFeed", hidenUsersFromFeed);
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: loggedInUser._id } },
+        { _id: { $nin: Array.from(hidenUsersFromFeed) } },
+      ],
+    }).select(User_Safe_Data)
+    .skip(skip)
+    .limit(limit);
+
+    res.json({ data: users }); 
   } catch (error) {
     error.status(400).json({ message: error.message });
   }
