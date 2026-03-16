3ec45ddf4303fad90f7901f6f4ceacd0f65d736c
PREETCHAUHAN2005 <preet.24b0101197@abes.ac.in>
2026-03-16 13:22:56 +0530

    changed mongodb password

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://preetchauhan271105_db_user:yoI1dPJWCo9sqN42@dev.qsovvuh.mongodb.net/Dev"
+    "mongodb+srv://preetchauhan271105_db_user:RC9dbDnfWPPq2wi2@dev.qsovvuh.mongodb.net/Dev"
   );
 };
 module.exports = connectDB;
