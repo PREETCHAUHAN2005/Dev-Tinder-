b325620630d9932cc38f9c9f83741c6e24399c12
PREETCHAUHAN2005 <preet.24b0101197@abes.ac.in>
2026-03-23 01:04:57 +0530

    feat:implemented email scheduling by cron job

--- backend/src/utils/cronjob.js
+++ backend/src/utils/cronjob.js
@@ -1,24 +1,33 @@
 const cron = require("node-cron");
-const { getDays, startodDay, endofDay } = require("data-fns");
-const ConnectionRequest = require("../models/connectionRequest");
-cron.schedule("* * * * * *", () => {
+const { subDays, startOfDay, endOfDay } = require("data-fns");
+const sendEmail = require("./sendEmail");
+const ConnectionRequestModel = require("../models/connectionRequest")
+// const ConnectionRequest = require("../models/connectionRequest");
+cron.schedule("0 8 * * *", async () => {
   //   console.log("Hello YC ," + new Date());
-try {
-    yestarday =subDays(new Date(), 1);
+  try {
+    yestarday = subDays(new Date(), 1);
 
-  const yesterdayStart = startodDay(yestarday);
-  const yesterdayEnd = endofDay(yestarday);
+    const yesterdayStart = startOfDay(yestarday);
+    const yesterdayEnd = endOfDay(yestarday);
 
-  const pendingRequests = ConnectionRequestModel.find({
-    status: "interested",
-    createdAt: {
-      $gte: yesterdayStart,
-      $lt: yesterdayEnd,
-    },
-  });
-    
-} catch (error) {
-    
-}
-  
+    const pendingRequests = await ConnectionRequestModel.find({
+      status: "interested",
+      createdAt: {
+        $gte: yesterdayStart,
+        $lt: yesterdayEnd,
+      },
+    }).populate("fromUserId toUserId");
+
+    const listOfEmails = new Set(
+      pendingRequests.map((req) => req.toUserId.emailId)
+    );
+    for (const email of listOfEmails) {
+      const res = await sendEmail.run(
+        "New friend Request pending for "+ email + 
+        "There are so many friend request pending ,please login to devtinder.in and accept all the friend request");
+    }
+  } catch (err) {
+    console.error(err); 
+  }
 });
